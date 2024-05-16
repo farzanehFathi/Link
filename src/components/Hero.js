c0e4e0d44f83a018e73ee2571f8818b9e1d029fa
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-05-16 19:32:53 +0330

    created alternating pictures

---
+++ src/components/Hero.js
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+// Import photos
+import autumn from "../imgs/Home/autumn.jpg";
+import coffee from "../imgs/Home/coffee.jpg";
+import diaries from "../imgs/Home/diaries.jpg";
+import laptop from "../imgs/Home/laptop.jpg";
+import life from "../imgs/Home/life.jpg";
+import mags from "../imgs/Home/mags.jpg";
+import mailbox from "../imgs/Home/mailbox.jpg";
+import notebook from "../imgs/Home/notebook.jpg";
+
+function AlternateImage({ imageUrls, currentImage }) {
+  const imageSrc = imageUrls[currentImage % imageUrls.length];
+  return (
+    <img
+      src={imageSrc}
+      alt="Alternating Photos"
+      className="object-cover h-dvh w-full"
+    />
+  );
+}
+const Hero = () => {
+  const [imageUrls, setImageUrls] = useState([
+    autumn,
+    coffee,
+    diaries,
+    laptop,
+    life,
+    mags,
+    mailbox,
+    notebook,
+  ]);
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentImage((prevCurrentImage) =>
+        prevCurrentImage === imageUrls.length - 1 ? 0 : prevCurrentImage + 1
+      );
+    }, 5000);
+    return () => clearInterval(intervalId);
+  }, [imageUrls]);
+
+  return (
+    <div>
+      <AlternateImage imageUrls={imageUrls} currentImage={currentImage} />
+    </div>
+  );
+};
+
+export default Hero;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,42 @@
-import autumn from "./autumn.jpg";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
     <div className="">
-      <div>
-        <img src={autumn} alt="" className="object-cover h-dvh w-full" />
+      <Hero />
+      <div className="bg-bg1">
+        <div className="py-100px w-90p max-w-1800px mx-auto flex justify-between items-top">
+          <p className="text-6xl w-8/12">
+            Fari is a multifaceted individual enthusiastic about building
+            technologies, web, and content.
+          </p>
+          <div className="text-3xl rotate-90">
+            <h2>
+              About Fari
+              <span> &#8594;</span>
+            </h2>
+          </div>
+        </div>
       </div>
-      <div className="bg-bg1 p-100px flex justify-around items-top">
-        <p className="text-5xl w-8/12">
-          Fari is a multifaceted individual enthusiastic about building energy
-          modelling, architecture and BIM, tech and web development,
-          copywriting, and content creation .
-        </p>
+      <div className="w-90p max-w-1800px mx-auto flex justify-between items-top">
+        <div className="text-5xl ">
+          <h2 className="my-12">Architecture</h2>
+          <h2 className="mb-6">
+            <span>&#8594; </span>Energy Modellings
+          </h2>
+          <h2 className="mb-6">
+            <span>&#8594; </span>Revit Architecture Works
+          </h2>
+          <h2 className="mb-6">
+            <span>&#8594; </span>Revit Structure Works
+          </h2>
+          <h2 className="mb-6">
+            <span>&#8594; </span>Research Studies
+          </h2>
+        </div>
         <div className="text-3xl rotate-90">
           <h2>
-            About Fari
+            Architecture
             <span> &#8594;</span>
           </h2>
         </div>
