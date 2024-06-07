043b34bc30af8b87ab27e6dc3e7c9318a244c5e6
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-06-07 13:33:14 +0330

    updated all services section

--- src/components/Home/AllServicesSection.js
+++ src/components/Home/AllServicesSection.js
@@ -47,24 +47,48 @@ const AllServicesSection = () => {
                   environment more resistant to climage change.
                 </p>
               </div>
+
+              <div className="group/showText w-10/12 ">
+                <h2 className="mb-4 group hover:opacity-50 hover:translate-x-4 transform transition-transform duration-300">
+                  <span className="span-x-translate">&#8594; </span>
+                  Sustainability
+                </h2>
+                <p className="absolute top-0 right-0 text-xl w-5/12 opacity-0 group-hover/showText:opacity-100 transition duration-300">
+                  Sustainability is no longer about making less bad choices, but
+                  about maxmising the positive impact. We have analysed the
+                  societal challenges that offer the greatest opportunity for us
+                  to contribute to UN's global goals for sustianable
+                  development, and defined how they can be applied in projects.
+                </p>
+              </div>
+
               <div className="group/showText w-10/12 ">
                 <h2 className="mb-4 group hover:opacity-50 hover:translate-x-4 transform transition-transform duration-300">
                   <span className="span-x-translate">&#8594; </span>
                   Transformation
                 </h2>{" "}
                 <p className="absolute top-0 right-0 text-xl w-5/12 opacity-0 group-hover/showText:opacity-100 transition duration-300">
-                  LINK represents one of the foremost envieronments within
-                  Landscapre Architecture in Scandinavia. In 2021 the studio was
-                  honored as "Best Scandinavian Landscape Architectrue Office".
-                  Our amibition is to create multifunctional, inclusive outdooer
-                  spaces that enrich bilogical diversity and make the
-                  environment more resistant to climage change.
+                  Architecture is about transforming the context of cities,
+                  places, or buildings to be preserved and filled with new life;
+                  to convert existing buildings and areas to better utilization
+                  and new functions. To give old buildings a boost and fill them
+                  with new content. A change for the better.
                 </p>
               </div>
-              <h2 className="mb-4 w-10/12">
-                <span className="span-x-translate">&#8594; </span>Digital
-                practice
-              </h2>
+
+              <div className="group/showText w-10/12 ">
+                <h2 className="mb-4 group hover:opacity-50 hover:translate-x-4 transform transition-transform duration-300">
+                  <span className="span-x-translate">&#8594; </span>
+                  Digital practice
+                </h2>{" "}
+                <p className="absolute top-0 right-0 text-xl w-5/12 opacity-0 group-hover/showText:opacity-100 transition duration-300">
+                  LINK is at the forefront of digital development, enabling us
+                  to create sustainable architecture from the early phases of a
+                  project through to completion. The customers will thus achieve
+                  full potential in any project, with the highest possible
+                  profit and the lowest possible risk.
+                </p>
+              </div>
             </div>
           </div>
         }
