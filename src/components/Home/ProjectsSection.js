025963d6922d93948861070ccb284d23a85d9a3a
Farzaneh <35253589+farzanehFathi@users.noreply.github.com>
2024-06-07 16:37:47 +0330

    created image container component

--- src/components/Home/ProjectsSection.js
+++ src/components/Home/ProjectsSection.js
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import SectionsPattern from "./SectionsPattern";
+import ImgContainer from "../ImgContainer";
 
 import Fllesskabets from "../../imgs/Home/F+ªllesskabets.jpg";
 import Norrtälje from "../../imgs/Home/Norrtälje.jpg";
@@ -12,59 +12,47 @@ const ProjectsSection = () => {
       <SectionsPattern
         content={
           <div className="flex flex-wrap w-11/12">
-            <div className="w-8/12 px-4 pb-12">
-              <Link>
-                <div className="relative w-max">
-                  <div className="bg-gray-800 z-50 absolute right-0 left-0 top-0 bottom-0 opacity-0 hover:opacity-40 transition-opacity ease-in-out delay-150"></div>
-                  <img
-                    className="object-fill"
-                    src={Skolekvartalet}
-                    alt="Ski school quarter"
-                  />
-                </div>
-                <h3 className="text-3xl my-2">Ski school quarter</h3>
-                <p className="text-lg">Urban development in the heart of Ski</p>
-              </Link>
-            </div>
+            <div className="w-full flex ">
+              <div className="w-8/12 px-4 pb-10">
+                <ImgContainer
+                  url={"#"}
+                  img={Skolekvartalet}
+                  alt={"Ski school quarter"}
+                  title={"Ski school quarter"}
+                  description={"Urban development in the heart of Ski"}
+                />
+              </div>
 
-            <div className="w-4/12 px-4 pb-12">
-              <Link>
-                <div className="relative ">
-                  <div className="bg-gray-800 z-50 absolute right-0 left-0 top-0 bottom-0 opacity-0 hover:opacity-40 transition-opacity ease-in-out delay-150"></div>
-                  <img src={Norrtälje} alt="Norrtälje Mortuary" />{" "}
-                </div>
-                <h3 className="text-3xl my-2">Norrtälje Mortuary</h3>
-                <p className="text-lg">
-                  Tasteful design for a dignified farewell
-                </p>
-              </Link>
+              <div className="w-4/12 px-4 pb-10">
+                <ImgContainer
+                  url={"#"}
+                  img={Norrtälje}
+                  alt={"Norrtälje Mortuary"}
+                  title={"Norrtälje Mortuary"}
+                  description={"Tasteful design for a dignified farewell"}
+                />
+              </div>
             </div>
+            <div className="w-full flex justify-between align-top">
+              <div className="w-4/12 px-4 py-10">
+                <ImgContainer
+                  url={"#"}
+                  img={Fllesskabets}
+                  alt={"Fællesskabets Hus"}
+                  title={"Fællesskabets Hus"}
+                  description={"A space for care and domesticity"}
+                />
+              </div>
 
-            <div className="w-5/12 px-6 py-12">
-              <Link>
-                <div className="relative">
-                  <div className="bg-gray-800 z-50 absolute right-0 left-0 top-0 bottom-0 opacity-0 hover:opacity-40 transition-opacity ease-in-out delay-150"></div>
-                  <img src={Fllesskabets} alt="Fællesskabets Hus" />{" "}
-                </div>
-                <h3 className="text-3xl my-2">Fællesskabets Hus</h3>
-                <p className="text-lg">A space for care and domesticity</p>
-              </Link>
-            </div>
-
-            <div className="w-7/12 px-6 py-12">
-              <Link>
-                <div className="relative w-max">
-                  <div className="bg-gray-800 z-50 absolute right-0 left-0 top-0 bottom-0 opacity-0 hover:opacity-40 transition-opacity ease-in-out delay-150"></div>
-                  <img
-                    src={Stovnertårnet}
-                    alt="Stovnertårnet - Stovner tower"
-                  />{" "}
-                </div>
-                <h3 className="text-3xl my-2">Stovnertårnet - Stovner tower</h3>
-                <p className="text-lg">
-                  Norway's most inclusive landscape project
-                </p>
-              </Link>
+              <div className="w-7/12 px-4 py-10">
+                <ImgContainer
+                  url={"#"}
+                  img={Stovnertårnet}
+                  alt={"Stovnertårnet - Stovner tower"}
+                  title={"Stovnertårnet - Stovner tower"}
+                  description={" Norway's most inclusive landscape project"}
+                />
+              </div>
             </div>
           </div>
         }
